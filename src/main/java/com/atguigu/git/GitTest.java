7b9660b6fb474db30c85aadab65d5fb3a3cc8755
wzhua <wzhua@163.com>
2021-11-18 21:51:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author wzhua
+ * @description
+ * @create 2021-11-18 21:47
+ */
+public class GitTest {
+  public static void main(String[] args) {
+    System.out.println("hello git");
+  }
+}
